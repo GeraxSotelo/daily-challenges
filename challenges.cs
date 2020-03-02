7bd783a44c66eb278fb7cf3be84f8f4e24854857
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-02 12:06:25 -0700

    Square matrix. Calculate abs diff b/w sums of diagonals

--- challenges.cs
+++ challenges.cs
@@ -17,3 +17,21 @@ static long aVeryBigSum(long[] ar)
   long total = ar.Sum();
   return total;
 }
+
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+// For example, the square matrix arr is shown below:
+// 1 2 3
+// 4 5 6
+// 9 8 9  
+// The left-to-right diagonal = 1+5+9=15 The right to left diagonal = 3+5+9=17 Their absolute difference is |15-17|=2
+public static int diagonalDifference(List<List<int>> arr)
+{
+  int ld = 0;
+  int rd = 0;
+  for (int up = 0, down = arr.Count - 1; up < arr.Count && down >= 0; up++, down--)
+  {
+    ld += arr[up][up];
+    rd += arr[up][down];
+  }
+  return Math.Abs(ld - rd);
+}
--- challenges.js
+++ challenges.js
@@ -12,3 +12,19 @@ function aVeryBigSum(arr) {
 function aVeryBigSum(arr) {
   return arr.reduce((total, num) => total += num);
 }
+
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+// For example, the square matrix arr is shown below:
+// 1 2 3
+// 4 5 6
+// 9 8 9  
+// The left-to-right diagonal = 1+5+9=15 The right to left diagonal = 3+5+9=17 Their absolute difference is |15-17|=2
+function diagonalDifference(arr) {
+  let ld = 0
+  let rd = 0
+  for (let up = 0, down = arr.length - 1; up < arr.length && down >= 0; up++ , down--) {
+    ld += arr[up][up];
+    rd += arr[up][down];
+  }
+  return Math.abs(ld - rd);
+}
